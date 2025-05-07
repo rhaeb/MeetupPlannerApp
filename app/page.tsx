2d07fe0cdf582d341a50308d59354b1a4ce88984
Draketzy <133954086+Draketzy@users.noreply.github.com>
2025-05-07 11:44:59 +0800

    tangtang

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import HomeScreen from "./(tabs)"
-import React from "react"
-export default function Page() {
-  return <HomeScreen />
-}
--- types/router.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "expo-router" {
-    export type LinkProps = {
-      href: string; // Loosen the type for dynamic paths
-      asChild?: boolean;
-      replace?: boolean;
-      scroll?: boolean;
-    };
-  }
-  
