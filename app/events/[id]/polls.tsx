84a455ad0c13d14decfe202b7cf70727210360f2
Draketzy <133954086+Draketzy@users.noreply.github.com>
2025-05-31 18:50:10 +0800

    poll  change button color

--- app/events/[id]/polls.tsx
+++ app/events/[id]/polls.tsx
@@ -689,7 +689,7 @@ const styles = StyleSheet.create({
     marginRight: spacing.xxl,
   },
   newPollButton: {
-    backgroundColor: '#3B82F6',
+    backgroundColor: 'green',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
